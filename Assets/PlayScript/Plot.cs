3f54d31d99bda8c5e54912410333e4669d94fde7
Vomeee <158576443+Vomeee@users.noreply.github.com>
2024-09-20 01:23:52 +0900

    built-in -> URP

--- Assets/PlayScript/Plot.cs
+++ Assets/PlayScript/Plot.cs
@@ -26,6 +26,8 @@ public class Plot : MonoBehaviour
         
     }
 
+    Vector3 examplePostition = new Vector3(400f, 5000f, 400f);
+    Vector3 exampleRotation = new Vector3(0, 0, 0);
     // Update is called once per frame
     void Update()
     {
@@ -34,7 +36,8 @@ public class Plot : MonoBehaviour
         // 3초 이상 경과한 경우
         if (elapsedTime > 4.0f)
         {
-            GameObject enemy1 = Instantiate(fa26Prefab);
+            
+            GameObject enemy1 = Instantiate(fa26Prefab, examplePostition, Quaternion.Euler(exampleRotation) );
             EnemyAI enemyAI1 = enemy1.GetComponent<EnemyAI>();
             if (enemyAI1 != null ) 
             {
